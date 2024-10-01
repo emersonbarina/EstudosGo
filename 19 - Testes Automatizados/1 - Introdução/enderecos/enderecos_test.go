568ca01a303b981e6194cfe3dc0f3c6013aa0d5c
emersonbarina <emersonbarina@gmail.com>
2024-09-30 23:45:46 -0300

    Dicas comandos testes - nome do packge com _test

--- 19 - Testes Automatizados/1 - Introdução/enderecos/enderecos_test.go
+++ 19 - Testes Automatizados/1 - Introdução/enderecos/enderecos_test.go
@@ -1,6 +1,9 @@
-package enderecos
+package enderecos_test
 
-import "testing"
+import (
+	"introducao-testes/enderecos"
+	"testing"
+)
 
 type cenarioDeTeste struct {
 	enderecoInserido string
@@ -9,21 +12,38 @@ type cenarioDeTeste struct {
 
 func TestTipoDeEnderecp(t *testing.T) {
 
+	t.Parallel()
+
 	cenariosDeTeste := []cenarioDeTeste{
 		{"Rua ABC", "Rua"},
 		{"Avenida Paulista", "Avenida"},
 		{"Rodovia Mário Covas", "Rodovia"},
-		{"Alameda Bem Fica", "Alameda"},
-		{"Praça 7 de Setembro", "Praça"},
+		//{"Alameda Bem Fica", "Alameda"},
+		//{"Praça 7 de Setembro", "Praça"},
 		{"Estrada teste", "Estrada"},
-		{"", "Tipo Inválido"},
+		//{"", "Tipo Inválido"},
 	}
 
 	for _, cenario := range cenariosDeTeste {
-		tipoDeEnderecoRecebido := TipoDeEndereco(cenario.retornoEsperado)
+		tipoDeEnderecoRecebido := enderecos.TipoDeEndereco(cenario.retornoEsperado)
 		if tipoDeEnderecoRecebido != cenario.retornoEsperado {
 			t.Errorf("O tipo recebido é diferente do esperado! Esperava %s e recebeu %s", cenario.retornoEsperado, tipoDeEnderecoRecebido)
 		}
 	}
+	// Dicas de uso go test
+	// go test
+	// go test ./...   								  	- testar todos o pacotes
+	// go test -v       							    - cada função
+	// go test --cover   				    			- Exibe % de cobertura
+	// go test --coverprofile <name.txt>  - gera um arquivo com o resultado do teste
+	// go tool cover --func=<name.txt>    - Exibe o percentual por pacote
+	// go tool cover --html=<name.txt>    - Exibe de forma detalhada em html sobre onde não há cobertura de testes
 
+}
+
+func TestQualqu(t *testing.T) {
+	t.Parallel()
+	if 1 > 2 {
+		t.Errorf("Teste quebrou...")
+	}
 }
