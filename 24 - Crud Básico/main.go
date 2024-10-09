623a6236613c77ebd82c2a412f32fe1767688723
emersonbarina <emersonbarina@gmail.com>
2024-10-09 11:34:33 -0300

    Criando rota e método com função anônima

--- 24 - Crud Básico/main.go
+++ 24 - Crud Básico/main.go
@@ -12,6 +12,9 @@ func main() {
 	// CRUD - CREATE, READ, UPDATE, DELETE
 
 	router := mux.NewRouter()
+	router.HandleFunc("/usuarios", func(w http.ResponseWriter, r *http.Request) {
+
+	}).Methods(http.MethodPost)
 
 	fmt.Println("Escutando na porta 5001")
 	log.Fatal(http.ListenAndServe(":5001", router))
