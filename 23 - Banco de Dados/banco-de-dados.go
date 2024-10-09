6b3d39fa4da868c5e252d8ad92de39e6e1e71114
emersonbarina <emersonbarina@gmail.com>
2024-10-09 01:01:30 -0300

    db.Query

--- 23 - Banco de Dados/banco-de-dados.go
+++ 23 - Banco de Dados/banco-de-dados.go
@@ -10,18 +10,30 @@ import (
 
 func main() {
 	stringConexao := "root:root@tcp(0.0.0.0:3307)/devbook?charset=utf8&parseTime=True&loc=Local"
+	//stringConexao := "golang:golang@tcp(0.0.0.0:3307)/devbook?charset=utf8&parseTime=True&loc=Local"
 	//db, err:= sql.Open("mysql", "root:password@tcp(docker.for.mac.localhost:3306)/mydbname?parseTime=true")
 
 	db, erro := sql.Open("mysql", stringConexao)
 	if erro != nil {
+		// fmt.Println("Dentro do sql.Open")
 		log.Fatal(erro)
 	}
 	defer db.Close()
 
 	if erro = db.Ping(); erro != nil {
+		// fmt.Println("Dentro do db.Ping")
 		log.Fatal(erro)
 	}
 
 	fmt.Println("Conexão aberta")
+
+	linhas, erro := db.Query("select * from usuarios")
+
+	if erro != nil {
+		log.Fatal(erro)
+	}
+	defer linhas.Close()
+
+	fmt.Println(linhas)
 
 }
--- README.md
+++ README.md
@@ -19,6 +19,7 @@ USE devbook;
 
 CREATE TABLE usuarios( id int auto_increment primary key, nome varchar(50) not null, email varchar(50) not null ) ENGINE=INNODB;
 show tables;
+CREATE USER 'golang'@'localhost'IDENTIFIED BY 'golang'
 GRANT ALL PRIVILEGES ON devbook.* TO 'golang'@'localhost';
 
 mysql -u goolang -p
