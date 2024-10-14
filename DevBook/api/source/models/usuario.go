bedff021c3bf3259cdf2a409602df76bc26d63ca
emersonbarina <emersonbarina@gmail.com>
2024-10-14 11:32:26 -0300

    Mátodos de Validação e Formatação de Usuário - Criação

--- DevBook/api/source/models/usuario.go
+++ DevBook/api/source/models/usuario.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 // Usuario representa um usuário
 type Usuario struct {
@@ -10,4 +14,44 @@ type Usuario struct {
 	Email    string    `json:"email,omitempty"`
 	Senha    string    `json:"senha,omitempty"`
 	CriadoEm time.Time `json:"criadoEm,omitempty"`
+}
+
+// Preparar vai chamar os métodos para validar e formatar os campos de usuarios recebidos
+func (usuario *Usuario) Preparar() error {
+	usuario.formatar()
+	if erro := usuario.validar(); erro != nil {
+		return erro
+	}
+	return nil
+}
+
+func (usuario *Usuario) validar() error {
+	if usuario.Nome == "" {
+		return errors.New("o nome é obrigatório e não pode estar em branco")
+	}
+
+	if usuario.Nick == "" {
+		return errors.New("o apelido é obrigatório e não pode estar em branco")
+	}
+
+	if usuario.Email == "" {
+		return errors.New("o e-mail é obrigatório e não pode estar em branco")
+	}
+
+	if usuario.Senha == "" {
+		return errors.New("a senha é obrigatória e não pode estar em branco")
+	}
+
+	return nil
+}
+
+func (usuario *Usuario) formatar() {
+	usuario.Nome = strings.Trim(usuario.Nome, " ")
+	usuario.Nick = strings.Trim(usuario.Nick, " ")
+	usuario.Email = strings.Trim(usuario.Email, " ")
+	usuario.Senha = strings.Trim(usuario.Senha, " ")
+	usuario.Nome = strings.TrimSpace(usuario.Nome)
+	usuario.Nick = strings.TrimSpace(usuario.Nick)
+	usuario.Email = strings.TrimSpace(usuario.Email)
+	usuario.Senha = strings.TrimSpace(usuario.Senha)
 }
