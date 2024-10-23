9e33e5e23a8b5b6b265e45cf5e592b11c402480e
emersonbarina <emersonbarina@gmail.com>
2024-10-23 20:55:51 -0300

    Modelo publicacoes

--- DevBook/api/sql/sql.sql
+++ DevBook/api/sql/sql.sql
@@ -28,3 +28,17 @@ CREATE TABLE seguidores(
 
   primary key(usuario_id, seguidor_id)
 )
+
+CREATE TABLE publicacoes(
+  id int primary key AUTO_INCREMENT,
+  titulo varchar(50) not null,
+  conteudo varchar(300) not null,
+
+  autor_id int not null,
+  FOREIGN KEY (autor_id)
+  REFERENCES usuarios(id)
+  ON DELETE CASCADE,
+
+  curtidas int default 0,
+  criadoEm timestamp default current_timestamp
+) ENGINE=INNODB;
