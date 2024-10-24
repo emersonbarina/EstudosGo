a86544dbc8b08eebac95dbf05cee889277f30f2d
emersonbarina <emersonbarina@gmail.com>
2024-10-23 22:58:43 -0300

    Métodos para validação publicacoes

--- DevBook/api/source/models/publicacoes.go
+++ DevBook/api/source/models/publicacoes.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 // Publicacao representa um publicação feita por um usuário
 type Publicacao struct {
@@ -11,4 +15,33 @@ type Publicacao struct {
 	AutorNick uint64    `json:"autorNick,omitempty"`
 	Curtidas  uint64    `json:"curtidas"`
 	CriadaEm  time.Time `json:"criadaEm,omitempty"`
+}
+
+// Preparar vai chamar os métodos para validar e formatar os campos de publicacoes recebidas
+func (publicacao *Publicacao) Preparar() error {
+	publicacao.formatar()
+
+	if erro := publicacao.validar(); erro != nil {
+		return erro
+	}
+	return nil
+}
+
+func (publicacao *Publicacao) validar() error {
+	if publicacao.Titulo == "" {
+		return errors.New("o título é obrigatório e não pode estar em branco")
+	}
+
+	if publicacao.Conteudo == "" {
+		return errors.New("o conteúdo é obrigatório e não pode estar em branco")
+	}
+
+	return nil
+}
+
+func (publicacao *Publicacao) formatar() {
+	publicacao.Titulo = strings.Trim(publicacao.Titulo, " ")
+	publicacao.Conteudo = strings.Trim(publicacao.Conteudo, " ")
+	publicacao.Titulo = strings.TrimSpace(publicacao.Titulo)
+	publicacao.Conteudo = strings.TrimSpace(publicacao.Conteudo)
 }
