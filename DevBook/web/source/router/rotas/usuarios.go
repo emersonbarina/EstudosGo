6688e4c6e5dec4e149efdd76a9a07636644382bf
emersonbarina <emersonbarina@gmail.com>
2024-10-29 00:12:30 -0300

    Página de Cadastro de Usuários

---
+++ DevBook/web/source/router/rotas/usuarios.go
@@ -0,0 +1,15 @@
+package rotas
+
+import (
+	"net/http"
+	"webapp/source/controllers"
+)
+
+var rotasUsuarios = []Rota{
+	{
+		URI:                "/criar-usuario",
+		Metodo:             http.MethodGet,
+		Funcao:             controllers.CarregarPaginaDeCadastroDeUsuario,
+		RequerAutenticacao: false,
+	},
+}
