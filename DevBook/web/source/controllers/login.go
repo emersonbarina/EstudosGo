10f2fc32d03d71d8e6617b7e710b91a994dee641
emersonbarina <emersonbarina@gmail.com>
2024-10-31 00:36:52 -0300

    Autenticando Loogin - Criando controller e js

---
+++ DevBook/web/source/controllers/login.go
@@ -0,0 +1,8 @@
+package controllers
+
+import "net/http"
+
+// FazerLogin utiliza o email e senha do usuário para autenticar na aplicação
+func FazerLogin(w http.ResponseWriter, r *http.Request) {
+
+}
