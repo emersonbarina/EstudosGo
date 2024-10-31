ec2d76b08c9f54770ccf7f7aafb45d7c2d89e16e
emersonbarina <emersonbarina@gmail.com>
2024-10-31 00:47:15 -0300

    Autenticado Login - Controller

--- DevBook/web/source/controllers/login.go
+++ DevBook/web/source/controllers/login.go
@@ -1,8 +1,36 @@
 package controllers
 
-import "net/http"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"webapp/source/responses"
+)
 
 // FazerLogin utiliza o email e senha do usuário para autenticar na aplicação
 func FazerLogin(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
 
+	usuario, erro := json.Marshal(map[string]string{
+		"email": r.FormValue("email"),
+		"senha": r.FormValue("senha"),
+	})
+
+	if erro != nil {
+		responses.JSON(w, http.StatusBadRequest, responses.ErroAPI{Erro: erro.Error()})
+		return
+	}
+
+	response, erro := http.Post("http://localhost:5001/login", "application/json", bytes.NewBuffer(usuario))
+	if erro != nil {
+		responses.JSON(w, http.StatusInternalServerError, responses.ErroAPI{Erro: erro.Error()})
+		return
+	}
+	defer response.Body.Close()
+
+	token, _ := io.ReadAll(response.Body)
+
+	fmt.Println(response.StatusCode, string(token))
 }
