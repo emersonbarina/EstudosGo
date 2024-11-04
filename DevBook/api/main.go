d41311410e20fa1ff2d185714592c7385f57ae3f
emersonbarina <emersonbarina@gmail.com>
2024-11-04 19:24:41 -0300

    Gerando chave randômica

--- DevBook/api/main.go
+++ DevBook/api/main.go
@@ -8,6 +8,18 @@ import (
 	"net/http"
 )
 
+// func init() {
+// 	// Gerando um chave randomica para o Secret_key
+// 	chave := make([]byte, 64)
+
+// 	if _, erro := rand.Read((chave)); erro != nil {
+// 		log.Fatal(erro)
+// 	}
+
+// 	stringBase64 := base64.StdEncoding.EncodeToString(chave)
+// 	fmt.Println(stringBase64)
+// }
+
 func main() {
 	config.Carregar()
 	// fmt.Println(config.StringConexaoBanco)
