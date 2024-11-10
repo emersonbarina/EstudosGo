989c304982e61ea065df7e38d9879e1f8a29dffe
emersonbarina <emersonbarina@gmail.com>
2024-11-10 11:52:59 -0300

    Função parar de seguir - Criando a função da rota em controllers

--- DevBook/web/source/controllers/usuarios.go
+++ DevBook/web/source/controllers/usuarios.go
@@ -5,8 +5,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 	"webapp/source/config"
+	"webapp/source/requests"
 	"webapp/source/responses"
+
+	"github.com/gorilla/mux"
 )
 
 // CriarUsuario chama a API para cadastrar um usuário no banco de dados
@@ -33,6 +37,31 @@ func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 	response, erro := http.Post(url, "application/json", bytes.NewBuffer(usuario))
 	if erro != nil {
 		// log.Fatal(erro)
+		responses.JSON(w, http.StatusInternalServerError, responses.ErroAPI{Erro: erro.Error()})
+		return
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode >= 400 {
+		responses.TratarStatusCodeDeErro(w, response)
+		return
+	}
+
+	responses.JSON(w, response.StatusCode, nil)
+}
+
+// PararDeSeguirUsuario chapa a API para parar de seguir um usuário
+func PararDeSeguirUsuario(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
+	usuarioID, erro := strconv.ParseUint(parametros["usuarioId"], 10, 64)
+	if erro != nil {
+		responses.JSON(w, http.StatusBadRequest, responses.ErroAPI{Erro: erro.Error()})
+		return
+	}
+
+	url := fmt.Sprintf("%s/usuarios/%d/parar-de-seguir", config.APIURL, usuarioID)
+	response, erro := requests.FazerRequisicaoComAutenticacao(r, http.MethodPost, url, nil)
+	if erro != nil {
 		responses.JSON(w, http.StatusInternalServerError, responses.ErroAPI{Erro: erro.Error()})
 		return
 	}
